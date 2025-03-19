803daa7153cda1a4fe49ceed1b8327776b8021cb
Azri <azriauliaakmal@gmail.com>
2025-03-19 10:46:29 +0700

    update script.js

--- color-picker/script.js
+++ color-picker/script.js
@@ -1,13 +1,13 @@
 // seleksi input color dan ketika color dipilih lakukan fungsi berikut
-document.getElementById("colorInput").addEventListener("input", (e) => {
+document.getElementById("colorInput").addEventListener("input", (event) => {
   // ambil input colornya
-  let colorSelected = e.target.value;
+  let selectedColor = event.target.value;
 
   // seleksi color code terus ubah textnya
   let colorCode = document.getElementById("colorCode");
-  colorCode.textContent = colorSelected.toUpperCase();
+  colorCode.textContent = selectedColor.toUpperCase();
 
   // ganti color displaynya
   let colorDisplay = document.getElementById("colorDisplay");
-  colorDisplay.style.backgroundColor = colorSelected;
+  colorDisplay.style.backgroundColor = selectedColor;
 });
